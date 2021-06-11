9c0d3941335500fb8de1b3cfea32f144a5d636ef
nicoleliano <56807477+nicoleliano@users.noreply.github.com>
2021-06-11 12:23:19 -0400

    added icon element

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ function displayWeather(response) {
   let currentConditions = document.querySelector("#currentConditions");
   let dayElement = document.querySelector("#day");
   let humidityOut = document.querySelector('#humidity');
+  let iconElement = document.querySelector('#icon');
   let temperature = document.querySelector("#currentTemperature");
   let windSpeed = document.querySelector("#wind");
   
@@ -24,10 +25,10 @@ function displayWeather(response) {
   currentConditions.innerHTML = response.data.weather[0].description;
   dayElement.innerHTML = date(response.data.dt * 1000);
   humidityOut.innerHTML = Math.round(response.data.main.humidity);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   temperature.innerHTML = Math.round(response.data.main.temp);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
-
-  console.log(response.data);
 };
 
 let apiKey = `1b4633177fe295b77ce7fe4928580db0`;
@@ -35,5 +36,4 @@ let city = `New York`;
 let units = `imperial`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayWeather);
