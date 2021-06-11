277a9a68caca66785df3033bfc451c65507d058c
nicoleliano <56807477+nicoleliano@users.noreply.github.com>
2021-06-11 12:40:12 -0400

    added search function

--- src/app.js
+++ src/app.js
@@ -31,9 +31,21 @@ function displayWeather(response) {
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
 };
 
-let apiKey = `1b4633177fe295b77ce7fe4928580db0`;
-let city = `New York`;
-let units = `imperial`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function search(city) {
+  let apiKey = `1b4633177fe295b77ce7fe4928580db0`;
+  let units = `imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(displayWeather);
+  axios.get(apiUrl).then(displayWeather);
+};
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#searchForm");
+  search(cityInput.value);
+};
+
+search("New York");
+
+let form = document.querySelector("#search");
+form.addEventListener("submit", searchCity);
