3c3fbd7a8ba9660f12caed14e26d10eefc2d1527
nicoleliano <56807477+nicoleliano@users.noreply.github.com>
2021-06-14 19:46:46 -0400

    added forecast element

--- src/app.js
+++ src/app.js
@@ -12,6 +12,54 @@ function date(timestamp) {
     return `${day} ${hours}:${minutes}`;
 };
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Weds", "Thu", "Fri", "Sat"];
+
+  return days[day];
+};
+
+function displayForecast(response) {
+let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 5) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col-2">
+    <div class="forecast-date">
+    ${formatDay(forecastDay.dt)}
+    </div>
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="72">
+        <div class="forecast-temp">
+        <span class="forecast-temp-max">
+        ${Math.round(forecastDay.temp.max)}°  
+        </span>
+        <span class="forecast-temp-min">
+        ${Math.round(forecastDay.temp.min)}°
+        </span>
+        </div>
+      </div>
+      `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  };
+
+
+function getForecast(coord) {
+  let apiKey = `1b4633177fe295b77ce7fe4928580db0`;
+  let units = `imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=${units}`;
+  
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
   let cityName = document.querySelector("#city");
   let currentConditions = document.querySelector("#currentConditions");
@@ -29,6 +77,8 @@ function displayWeather(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   temperature.innerHTML = Math.round(response.data.main.temp);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  getForecast(response.data.coord);
 };
 
 function search(city) {
